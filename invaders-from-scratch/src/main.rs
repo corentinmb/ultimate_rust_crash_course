162f7024ea3dcb2b13a336c180aabb2710ff4b56
Corentin Moreau <corentin.moreau@doctolib.com>
2024-03-22 17:12:28 +0100

    loop and gameboard

--- invaders-from-scratch/src/main.rs
+++ invaders-from-scratch/src/main.rs
@@ -1,39 +1,22 @@
+use cargo::frame::new_frame;
+use cargo::{frame, render};
 use crossterm::cursor::{Hide, Show};
+use crossterm::event::{Event, KeyCode};
 use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
-use crossterm::{terminal, ExecutableCommand};
+use crossterm::{event, terminal, ExecutableCommand};
 use rusty_audio::Audio;
 use std::error::Error;
-use std::io;
 use std::io::Stdout;
+use std::sync::mpsc;
+use std::time::Duration;
+use std::{io, thread};
 
-type InvadersResult = Result<(), Box<dyn Error>>;
+pub type InvadersResult = Result<(), Box<dyn Error>>;
 
 fn main() -> InvadersResult {
     let mut audio = Audio::new();
     let mut stdout = io::stdout();
 
-    init_sounds(&mut audio);
-    init_terminal(&mut stdout);
-    cleanup(&mut audio, &mut stdout);
-    Ok(())
-}
-
-fn cleanup(audio: &Audio, stdout: &mut Stdout) -> InvadersResult {
-    audio.wait();
-    stdout.execute(Show)?;
-    stdout.execute(LeaveAlternateScreen)?;
-    terminal::disable_raw_mode()?;
-    Ok(())
-}
-
-fn init_terminal(stdout: &mut Stdout) -> InvadersResult {
-    terminal::enable_raw_mode()?;
-    stdout.execute(EnterAlternateScreen)?;
-    stdout.execute(Hide)?;
-    Ok(())
-}
-
-fn init_sounds(audio: &mut Audio) -> InvadersResult {
     let sounds = ["explode", "lose", "move", "pew", "startup", "win"];
 
     for sound in sounds.iter() {
@@ -41,5 +24,54 @@ fn init_sounds(audio: &mut Audio) -> InvadersResult {
         audio.add(sound, &path);
     }
     audio.play("startup");
+
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
+    // Render loop in a separate thread
+    let (render_tx, render_rx) = mpsc::channel();
+    let render_handle = thread::spawn(move || {
+        let mut last_frame = new_frame();
+        let mut stdout = io::stdout();
+        render::render(&mut stdout, &last_frame, &last_frame, true);
+
+        loop {
+            let curr_frame = match render_rx.recv() {
+                Ok(x) => x,
+                Err(_) => break,
+            };
+            render::render(&mut stdout, &last_frame, &curr_frame, false);
+            last_frame = curr_frame;
+        }
+    });
+
+    // Game loop
+    'gameloop: loop {
+        let curr_frame = new_frame();
+
+        while event::poll(Duration::default())? {
+            if let Event::Key(key) = event::read()? {
+                match key.code {
+                    KeyCode::Char('q') | KeyCode::Esc => {
+                        audio.play("lose");
+                        break 'gameloop;
+                    }
+                    _ => {}
+                }
+            }
+        }
+        // Draw & render
+        let _ = render_tx.send(curr_frame);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    drop(render_tx);
+    render_handle.join().unwrap();
+    audio.wait();
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
+
     Ok(())
 }
